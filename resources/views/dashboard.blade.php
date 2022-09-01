241b657aa8a73b301527be5898dfb569e3ca0ebe
AkashMarkad <akashmarkad2210@gmail.com>
2022-09-01 22:32:15 +0530

    Updated

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,44 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+        <div class="container">
+            <div class="row">
+                <div class="col-3 p-5">
+                    <img src="/IMG/Logo.png" style="height:100px;" alt="">
+                </div>
+                <div class="col-9">
+                    <div><h1>CodingElite</h1></div>
+                    <div class="flex px-10">
+                        <div class="pr-5"><strong>30</strong> Posts</div>
+                        <div class="pr-5"><strong>20k</strong> Followers</div>
+                        <div class="pr-5"><strong>500</strong> Following</div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
+    </x-slot>
+
 </x-app-layout>
+
+
+
+<!-- @section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{session('status')}}
+                        </div>
+                    @endif
+
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>  -->
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -4,18 +4,23 @@
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
+                <!-- <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
                         <x-jet-application-mark class="block h-9 w-auto" />
                     </a>
-                </div>
+                </div> -->
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                <!-- <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
+                </div> -->
+                <div class="navbar-brand flex py-5" href="{{ url('/')}}">
+                    <div><img src="/IMG/Logo.png" alt="" style="height:25px; border-right:1px solid #000000;"></div>
+                    <div class="px-2">Instagram</div>
                 </div>
+            
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ml-6">
@@ -75,16 +80,20 @@
                         <x-slot name="trigger">
                             @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
                                 <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                                
                                     <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                                    
                                 </button>
                             @else
                                 <span class="inline-flex rounded-md">
+                                
                                     <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition">
                                         {{ Auth::user()->name }}
 
                                         <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                                             <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                                         </svg>
+                                        
                                     </button>
                                 </span>
                             @endif
